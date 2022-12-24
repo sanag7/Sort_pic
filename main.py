2d50fb009cd66024ae5c76562ad4d9b575ddd1dd
이환주 <hwanju@ihwanjuui-MacBook-Pro.local>
2022-12-24 15:11:41 +0900

    Update main.py
    1.Device별 날짜및 시간 경로 추가
    2.날짜 시간 없는 파일 생성 시간으로 수정

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import os
 from PIL import Image
 import shutil
+from tqdm import tqdm
+import datetime
 
 
-PIC_PATH = "/Users/hwanju/Desktop/Python workspace/PicSort"
+# PIC_PATH = "E:\\Pic_all"  #E:\Pic_all
+# SAVE_PATH = "E:\\Pic_Sort"
+
+PIC_PATH = "/Users/hwanju/Desktop/Python workspace/PicSort/pic_raw"
 SAVE_PATH = "/Users/hwanju/Desktop/Python workspace/PicSort/save_pic"
 
 pic_ext_list = [".jpg", ".jpeg", ".jpe",".gif" ".png" ]
+DVICE_LIST = ['Apple', 'SAMSUNG', 'samsung', 'KTH' , 'Snowcorp', 'Canon', 'SONY', 'NIKON CORPORATION', 'Panasonicclear']
 
 pic_file_path_list = []
 # file_list = os.listdir(PIC_PATH)
@@ -22,16 +28,23 @@ for (root, dirs, files) in os.walk(PIC_PATH):
                 if ext.lower() in pic_ext_list:
                     pic_file_path_list.append([root,basename,ext])
                 else:
-                    print("대문자")
+                    print(ext)
 
-for dir, basename, ext in pic_file_path_list:
+for dir, basename, ext in tqdm(pic_file_path_list):
     file_name = basename + ext
     pic_file_path = os.path.join(dir,basename + ext)
     img = Image.open(pic_file_path)
-    meta_data = img._getexif()
+    meta_data = img.getexif()
+    # print(meta_data)
     
+
+
     try:  # 사진은 월별로 저장
-        make_time = meta_data[36867]
+        if meta_data[271] in DVICE_LIST:
+            make_time = meta_data[306]
+        
+        else:
+            make_time = meta_data[36867]
         year = make_time[0:4]
         month = make_time[5:7]
         day = make_time[8:10]
@@ -58,7 +71,7 @@ for dir, basename, ext in pic_file_path_list:
             cnt += 1
             new_path  = os.path.join(month_dir,new_name + "_(" + str(cnt) + ")" + ext)
         
-        shutil.move(pic_file_path,new_path)
+        shutil.copy2(pic_file_path,new_path)
 
         
 
@@ -66,6 +79,33 @@ for dir, basename, ext in pic_file_path_list:
 
     except: # None 폴더로 모이도록 조정
         # pass
-        none_dir = os.path.join(SAVE_PATH,"None")
-        new_path  = os.path.join(none_dir,file_name)
-        shutil.move(pic_file_path,new_path)
+        ctime = os.path.getctime(pic_file_path)
+        rtime = datetime.datetime.fromtimestamp(ctime).strftime("%Y_%m_%d %H_%M_%S")
+
+        year = rtime[0:4]
+        month = rtime[5:7]
+
+        year_dir = os.path.join(SAVE_PATH,year)
+        month_dir = os.path.join(year_dir,month)
+
+        if os.path.exists(year_dir):
+            pass
+        else:
+            os.mkdir(year_dir)
+        
+        if os.path.exists(month_dir):
+            pass
+        else:
+            os.mkdir(month_dir)
+
+        
+        cnt = 0
+        new_path  = os.path.join(month_dir,rtime + "_(" + str(cnt) + ")" + ext)
+        while os.path.exists(new_path):
+            cnt += 1
+            new_path  = os.path.join(month_dir,rtime + "_(" + str(cnt) + ")" + ext)
+
+        # none_dir = os.path.join(SAVE_PATH,"None")
+        # print(meta_data)
+        # new_path  = os.path.join(none_dir,file_name)
+        shutil.copy2(pic_file_path,new_path)
